b10b2ad59289fed542cf0d3e29eab3e9b592f905
OptimusSime <simonjdaniel@gmail.com>
2018-04-01 17:07:19 +1000

    Modified CreateTables.sql - Now two tables Sales & Products Creates PopulateTestData.sql - adds two lines of data to both Sales and Products Created ReadProducts.sql & ReadSales.sql - demonstrates READ

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,31 +1,21 @@
-﻿DROP TABLE PRODUCT;
-DROP TABLE INVENTORY;
-DROP TABLE SALESTXNS;
+﻿DROP TABLE SALES;
+DROP TABLE PRODUCTS;
 
 
-Create Table PRODUCT(
-	productid		NUMERIC(8),
+Create Table PRODUCTS(
+	productid		int IDENTITY(1,1) PRIMARY KEY,
 	pname			VARCHAR(30),
 	pcost			DECIMAL(6,2),
-	CONSTRAINT PK_PRODUCT PRIMARY KEY (productid)
+	pstock			NUMERIC(6),
 );
 
-Create Table INVENTORY(
-	invid			NUMERIC(8),
-	productid		NUMERIC(8),
-	quantity		NUMERIC(6),
-	CONSTRAINT PK_INV PRIMARY KEY (invid),
-	FOREIGN KEY (productid) REFERENCES PRODUCT
-);
-
-Create Table SALESTXN(
-	txnid			NUMERIC(8),
+Create Table SALES(
+	saleid			int IDENTITY(1,1) PRIMARY KEY,
 	saledate		datetime,
-	productid		NUMERIC(8),
+	productid		int,
 	qtysold			NUMERIC(4),
 	saleprice		DECIMAL(6,2),
-	CONSTRAINT PK_SALESTXN PRIMARY KEY (txnid),
-	FOREIGN KEY (productid) REFERENCES PRODUCT,
+	FOREIGN KEY (productid) REFERENCES PRODUCTS,
 	CONSTRAINT CHK_SALESTXN_saleprice
 				CHECK ((saleprice >= 0) AND (saleprice <= 5000)),
 	CONSTRAINT CHK_SALESTXN_qtysold
