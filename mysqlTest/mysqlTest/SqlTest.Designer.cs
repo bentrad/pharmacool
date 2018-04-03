a1c6002a1cada3d1fb9d1420e36a46a0541b6e85
DreamCreateDreamAgain <lukegavin.5@outlook.com>
2018-04-03 18:58:09 +1000

    Added DB test connection code. This connection should work however I am having authentification problems with XAMPP

---
+++ mysqlTest/mysqlTest/SqlTest.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace mysqlTest
+{
+    partial class SqlTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lbl_title = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtMobile = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Lbl_title
+            // 
+            this.Lbl_title.AutoSize = true;
+            this.Lbl_title.Location = new System.Drawing.Point(12, 9);
+            this.Lbl_title.Name = "Lbl_title";
+            this.Lbl_title.Size = new System.Drawing.Size(74, 13);
+            this.Lbl_title.TabIndex = 0;
+            this.Lbl_title.Text = "MySql Testing\r\n";
+            this.Lbl_title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(12, 43);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 2;
+            // 
+            // txtMobile
+            // 
+            this.txtMobile.Location = new System.Drawing.Point(12, 125);
+            this.txtMobile.Name = "txtMobile";
+            this.txtMobile.Size = new System.Drawing.Size(100, 20);
+            this.txtMobile.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 82);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.AccessibleName = "btnSubmit";
+            this.btnSubmit.Location = new System.Drawing.Point(204, 125);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 5;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 184);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(408, 150);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // SqlTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 336);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtMobile);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.Lbl_title);
+            this.Name = "SqlTest";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.SqlTest_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl_title;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtMobile;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ mysqlTest/mysqlTest/SqlTest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace mysqlTest
+{
+    public partial class SqlTest : Form
+    {
+
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+
+            Application.Run(new SqlTest());
+
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+
+        // "Server=localhost;Database=testdb;Uid=root;Pwd=YES;";
+
+        //Azure server: pharmacool.database.windows.net ; Database: pcdb ; User: pharma ; password: pinapplepizza1!
+        string MyConnectionString = "Server=pharmacool.database.windows.net;Database=pcdb;Uid=pharma;Pwd=pineapplepizza1!;";
+
+        public SqlTest()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+
+            /*private*/
+            //void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+            //{
+
+            //}
+
+            //private void SqlTest_Load(object sender, EventArgs e)
+            //{
+            //}
+
+
+            private void LoadData()
+            {
+                MySqlConnection connection = new MySqlConnection(MyConnectionString);
+                connection.Open();
+                try
+                {
+                    MySqlCommand cmd = connection.CreateCommand();
+                    cmd.CommandText = "SELECT * FROM phonebook";
+                    MySqlDataAdapter adap = new MySqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    adap.Fill(ds);
+
+                    dataGridView1.DataSource = ds.Tables[0].DefaultView;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                int MobileNo;
+                string Mobile = txtMobile.Text;
+                int.TryParse(Mobile, out MobileNo);
+
+                MySqlConnection connection = new MySqlConnection(MyConnectionString);
+                MySqlCommand cmd;
+                connection.Open();
+                try
+                {
+                    cmd = connection.CreateCommand();
+                    cmd.CommandText = "INSERT INTO phonebook(Id,Name, MobileNo)VALUES(@Id,@Name,@MobileNo)";
+                    cmd.Parameters.AddWithValue("@Id", int.Parse(txtId.Text)); //TODO: add ID and Name(Maybe add via windows Forms?)
+                    cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@MobileNo", Mobile);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+
+                    if (connection.State == ConnectionState.Open)
+                    {
+                        connection.Close();
+                        LoadData();
+                    }
+
+                }
+            }
+        }
+
+    }
+}
