9d0d395e268800acbfde58c6d2f923efd2c3fe60
Bradley <101626151@student.swin.edu.au>
2018-04-24 16:20:05 +1000

    Added an interface, added a parent class for product and sales, updated ProdRecord.

---
+++ ProductRecord/ProductRecord/IHaveDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data.SqlClient;
+
+//using MySql.Data.MySqlClient;
+
+namespace ProductRecord
+{
+    interface IHaveDatabase
+    {
+        //delete
+        Boolean Remove(string[] value);
+
+        //edit
+        Boolean Edit(string[] value);
+
+        //create
+        Boolean Create(string[] value);
+    }
+}
